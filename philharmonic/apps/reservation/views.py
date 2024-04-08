2c1eb819148532d57f1fc795581e022349ecf908
Sparki-Fluffy <andrewdev276@gmail.com>
2024-04-08 18:42:49 +0700

    Финальные изменения по переделке поведения роутов.

--- philharmonic/apps/reservation/views.py
+++ philharmonic/apps/reservation/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def reservation(request):
-    return HttpResponse('Бронирование')
+    return render(request, 'reservation/index.html')
