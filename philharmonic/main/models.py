2f135675d62b48ceaa509980026906072a72996d
Sparki-Fluffy <andrewdev276@gmail.com>
2024-04-16 21:30:55 +0700

    Добавлены наброски таблиц в файле моделей models.py, прописанных в виде Python классов, внутрь которых необходимо будет прописать необходимые поля с соответствующими типами данных и связами.

--- philharmonic/main/models.py
+++ philharmonic/main/models.py
@@ -1,5 +1,33 @@
 from django.db import models
 
 
-class FirstModel(models.Model):
+class News(models.Model):
+    pass
+
+
+class Events(models.Model):
+    pass
+
+
+class Actors(models.Model):
+    pass
+
+
+class Users(models.Model):
+    pass
+
+
+class Filials(models.Model):
+    pass
+
+
+class Home(models.Model):
+    pass
+
+
+class Booking(models.Model):
+    pass
+
+
+class Forum(models.Model):
     pass
