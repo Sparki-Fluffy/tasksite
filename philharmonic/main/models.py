df7be8940dcc7fb628135941bf92b8f96b27b30b
Sparki-Fluffy <andrewdev276@gmail.com>
2024-04-17 09:58:54 +0700

    Изменено содержание моделей, добавлены данные в другие таблицы. Связи пока не настроены. Донастройка требуется.

--- philharmonic/main/models.py
+++ philharmonic/main/models.py
@@ -2,22 +2,35 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=35)
+    title = models.CharField(max_length=40)
     content = models.TextField()
     pub_date = models.DateField()
     event = models.IntegerField()
 
 
 class Events(models.Model):
-    pass
+    event_title = models.CharField(max_length=40)
+    event_date = models.DateField()
+    description = models.TextField()
+    category = models.CharField(max_length=40)
 
 
 class Actors(models.Model):
-    pass
+    surname = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    patronymic = models.CharField(max_length=30)
+    genre = models.CharField(max_length=20)
+
 
 
 class Users(models.Model):
-    pass
+    surname = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    patronymic = models.CharField(max_length=30)
+    # TODO: edit email and password field in table 'Users'
+    # email = models.CharField(max_length=120)
+    # password = models.CharField(max_length=120)
+    rolt = models.CharField(max_length=30)
 
 
 class Filials(models.Model):
