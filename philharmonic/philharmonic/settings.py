0ab203964f93642031ce5754f2269d6e2448c424
Sparki-Fluffy <andrewdev276@gmail.com>
2024-05-06 18:26:36 +0700

    Add option init command for connection to database.

--- philharmonic/philharmonic/settings.py
+++ philharmonic/philharmonic/settings.py
@@ -67,6 +67,9 @@ DATABASES = {
         'PASSWORD': '128432506',
         'HOST': '127.0.0.1',
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
